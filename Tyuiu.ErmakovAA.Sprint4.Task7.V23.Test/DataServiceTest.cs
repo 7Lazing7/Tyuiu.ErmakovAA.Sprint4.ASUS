a4d4cf756f48a820fda7607e01ba85f5e54a2d1e
alexe <alexe@LAPTOP-EPJKKTKU>
2024-11-14 14:30:16 +0500

    Add project files.

---
+++ Tyuiu.ErmakovAA.Sprint4.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.ErmakovAA.Sprint4.Task7.V23.Lib;
+namespace Tyuiu.ErmakovAA.Sprint4.Task7.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService dataService = new DataService();
+            Assert.AreEqual(2, dataService.Calculate(4, 3, "678135972584"));
+        }
+    }
+}
---
+++ Tyuiu.ErmakovAA.Sprint4.Task7.V23/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ErmakovAA.Sprint4.Task7.V23.Lib;
+namespace Tyuiu.ErmakovAA.Sprint4.Task7.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполгил: Ермаков А.А. | ПКТб-24-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                    *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                                  *");
+            Console.WriteLine("* Задание #0                                                                   *");
+            Console.WriteLine("* Вариант #6                                                                   *");
+            Console.WriteLine("* Выполнил: Ермаков А.А.                                                       *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            int[,] matrix = new int[4, 3];
+            string str = "678135972584";
+
+            int index = 0;
+            Console.WriteLine("\nМассив");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++, index++)
+                {
+                    Console.Write($"{str[index]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("Количество нечётных элементов = " + ds.Calculate(4, 3, str));
+            Console.ReadKey();
+        }
+    }
+}
